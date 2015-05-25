4c1c20ea2a17674b0dfd2fb75e1b19b009fab550
Sergi Tur <sergiturbadenas@gmail.com>
2015-05-25 17:14:28 +0200

    Initializing 2

--- src/acacha/LaravelPackageInstaller/LaravelPackageInstaller.php
+++ src/acacha/LaravelPackageInstaller/LaravelPackageInstaller.php
@@ -9,11 +9,22 @@
 
 namespace Acacha\LaravelPackageInstaller;
 
-use Composer\Package\PackageInterface;
 use Composer\Installer\LibraryInstaller;
+use Composer\Package\PackageInterface;
+use Composer\Repository\InstalledRepositoryInterface;
+
 
 class LaravelPackageInstaller extends LibraryInstaller
 {
+
+    /**
+     * {@inheritDoc}
+     */
+    public function install(InstalledRepositoryInterface $repo, PackageInterface $package)
+    {
+        $this->io->write('Installing Laravel Package ###########');
+
+    }
 
     /**
      * {@inheritDoc}
