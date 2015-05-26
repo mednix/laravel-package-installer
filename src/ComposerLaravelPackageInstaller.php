7fdc5c787e8a5ea13f53b37098bc244e4aaf365f
Sergi Tur <sergiturbadenas@gmail.com>
2015-05-26 19:50:41 +0200

    Sync

--- src/ComposerLaravelPackageInstaller.php
+++ src/ComposerLaravelPackageInstaller.php
@@ -24,7 +24,7 @@ class ComposerLaravelPackageInstaller extends LibraryInstaller
 
     public function __construct()
     {
-        $this->config = $config;
+        //$this->config = $config;
     }
 
 
