6db10d0eb5c31f42431ef9ea85b48559f174f474
Sergi Tur <sergiturbadenas@gmail.com>
2015-05-28 12:44:34 +0200

    Local Sync

--- src/ComposerLaravelPackageInstaller.php
+++ src/ComposerLaravelPackageInstaller.php
@@ -17,7 +17,7 @@ class ComposerLaravelPackageInstaller extends LibraryInstaller
     {
         $extra = $package->getExtra();
 
-        $this->io->write('extra: ' + var_dump($extra));
+        $this->io->write('extra: ' + var_export($extra));
 
         if (!empty($extra['laravel-providers'])) {
             // Add laravel providers to config/app.php providers vector
